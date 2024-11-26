d19fe221fc8deed26cfd265992c3d74a95fc25b3
Jan Maciejewski <maciejewski.jan.m@gmail.com>
2024-11-26 12:07:24 +0100

    added coloring on mouseover, styles, reset btn

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const container = document.querySelector(".gridContainer")
 
-// Create a 4by4 grid. First column of subcontainers
-for (let i = 0; i < 4; i++) {
+// Create a 16x16 grid. First column of subcontainers
+for (let i = 0; i < 160; i++) {
     subcontainer = document.createElement("div")
     subcontainer.classList.add('subcontainer')
     container.appendChild(subcontainer)
 
-    // Create 4 divs within as columns
-    for (let i = 0; i < 4; i++) {
+    // Create 16 divs within as columns
+    for (let i = 0; i < 160; i++) {
         const newDiv = document.createElement("div")
+        newDiv.classList.add('horizontalDiv')
+        subcontainer.appendChild(newDiv)
         
     }
-}
+};
+
+cells = document.querySelectorAll('.horizontalDiv');
+
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "green"
+    })
+})
+
+resetBtn = document.querySelector(".resetBtn")
+
+resetBtn.addEventListener("click", () => {
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = null
+    })
+})
