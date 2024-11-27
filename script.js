4d5ce2be469ac8e2f91fdf4cbea07a30027a9136
Jan Maciejewski <maciejewski.jan.m@gmail.com>
2024-11-27 15:26:22 +0100

    modified flexboxes, added opacity increment

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
-const container = document.querySelector(".gridContainer")
+const container = document.querySelector(".gridContainer");
+let gridSize = 16;
 
-// Create a 16x16 grid. First column of subcontainers
-for (let i = 0; i < 160; i++) {
-    subcontainer = document.createElement("div")
-    subcontainer.classList.add('subcontainer')
-    container.appendChild(subcontainer)
+// Initialize a button that prompts user to change grid size
+const gridSizeBtn = document.querySelector(".gridSizeBtn");
 
-    // Create 16 divs within as columns
-    for (let i = 0; i < 160; i++) {
-        const newDiv = document.createElement("div")
-        newDiv.classList.add('horizontalDiv')
-        subcontainer.appendChild(newDiv)
-        
-    }
-};
-
-cells = document.querySelectorAll('.horizontalDiv');
-
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "green"
-    })
+gridSizeBtn.addEventListener("click", () => {
+    let newSize = parseInt(prompt("Choose new grid base between 1 and 100"));
+    while (isNaN(newSize) || newSize > 100 || newSize < 1) {
+        newSize = prompt("Choose more wisely");
+    };    
+    gridSize = newSize;
+    theGrid = createGrid(gridSize);
 })
 
-resetBtn = document.querySelector(".resetBtn")
+// Create a 16x16 grid. First column of subcontainers
+function createGrid(size) {
+    // Clear the container contents
+    container.innerHTML = "";
+
+    for (let i = 0; i < size; i++) {
+        subcontainer = document.createElement("div");
+        subcontainer.classList.add('subcontainer');
+        container.appendChild(subcontainer);
+        
+        // Create 16 divs within as columns
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add('horizontalDiv');
+            subcontainer.appendChild(newDiv);
+            
+        }
+    }
+    // Select grid cells and listen for mouse hovering
+    cells = document.querySelectorAll('.horizontalDiv');
+    
+    cells.forEach((cell) => {
+        cell.style.opacity = 0.1;
+
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "green";
+            cell.style.opacity = parseFloat(cell.style.opacity) + 0.1
+        })
+    })
+    
+};
+
+
+createGrid(gridSize);
+
+const resetBtn = document.querySelector(".resetBtn");
 
 resetBtn.addEventListener("click", () => {
     cells.forEach((cell) => {
-        cell.style.backgroundColor = null
+        cell.style.backgroundColor = null;
     })
 })
